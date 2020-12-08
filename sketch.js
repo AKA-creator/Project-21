fe4382a0f893ae806901d0ba2ee9a7db2634bb72
AKA-creator <74938084+AKA-creator@users.noreply.github.com>
2020-12-07 20:33:33 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var bullet, wall, thickness;
+var speed, weight;
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223, 321);
+  weight = random(30, 52);
+  thickness = random(22, 83);
+  bullet = createSprite(50, 200, 20, 10);
+  wall = createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor = color(80, 80, 80);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "white";
+}
+
+function draw() {
+  background(0);
+  
+  if(bullet.isTouching(wall))
+  {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/ thickness * thickness * thickness;
+    
+    if(damage > 10){
+      bullet.shapeColor = color("red");
+    }
+    if(damage < 10){
+      bullet.shapeColor = color("green");
+    }
+  }
+  drawSprites();
+}
